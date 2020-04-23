e6031c4c2fc82938e00dcdbd22963558697940df
ssuni2 <ssuni2@illinois.edu>
2020-04-22 23:53:58 -0500

    commit?

---
+++ Downloads/cinder_0.9.2_mac/my-projects/final-project-ssuni2/DEVELOPMENT.md
@@ -0,0 +1,6 @@
+# Development
+
+Week 1:
+* Installed tensorflow and properly included it in the project
+* Working on C++ api to actually get tensorflow to work in C++
+---
---
+++ Downloads/cinder_0.9.2_mac/my-projects/final-project-ssuni2/src/kernel.h
@@ -0,0 +1,14 @@
+//
+// Created by Serena Suni on 4/22/20.
+//
+
+#ifndef TF_INFERENCE_KERNEL_H
+#define TF_INFERENCE_KERNEL_H
+
+
+class kernel {
+
+};
+
+
+#endif //TF_INFERENCE_KERNEL_H
