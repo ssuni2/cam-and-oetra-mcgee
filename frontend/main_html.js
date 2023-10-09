15208c9de0ea971e759243e15f4abe252f1baef0
EndlessEevee <mason@masoncucci.com>
2023-10-08 23:57:43 -0700

    Got rid blur & made it a panel instead of a window
    Got rid of all the blur, made it into a panel on the right side. I also got rid of close button. Can be added back if needed.

--- frontend/main_html.js
+++ frontend/main_html.js
@@ -15,9 +15,17 @@ function emailElement(from, date, subject, content) {
 
 function addEmail(from, date, subject, content) {
   let el = emailElement(from, date, subject, content);
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
+    // Close existing email window if open
+    if (emailWindow) {
+      document.body.classList.remove("has-modal");
+      document.body.removeChild(emailWindow);
+      emailWindow = null;
+    }
+
     createEmailWindow(el);
   });
+
   let emails = document.querySelector("#emails");
   emails.insertBefore(el, emails.querySelector(".emailPreview")?.parentElement);
 }
@@ -38,15 +46,15 @@ function createEmailWindow(el) {
   div.className = "emailWindow";
 
   div.innerHTML = `
-  <div class="emailView">
-    <span>From: </span><a class="sender"></a> 
-    <br>
-    <span class="subject"></span>
-    <hr>
-    <span class="content"></span>
-  </div>
-  <textarea class="emailCompose" placeholder="Compose a reply..."></textarea>
-  <button class="close">&#10006</button>
+    <div class="emailView">
+      <span>From: </span><a class="sender"></a> 
+      <br>
+      <span class="subject"></span>
+      <hr>
+      <span class="content"></span>
+    </div>
+    <textarea class="emailCompose" placeholder="Compose a reply..."></textarea>
+    <button class="close">&#10006</button>
   `;
 
   let [sender, subject, content] = [
@@ -57,14 +65,28 @@ function createEmailWindow(el) {
   div.querySelector(".sender").innerText = sender;
   div.querySelector(".sender").href = "mailto:" + sender;
   div.querySelector(".subject").innerText = "Subject: " + subject;
+
+  // Set CSS properties to position the email window on the right side
+  div.style.position = "fixed";
+  div.style.top = "0";
+  div.style.right = "0";
+  div.style.width = "65%"; // Adjust the width as needed
+  div.style.height = "100%";
+  div.style.backgroundColor = "white";
+  div.style.padding = "10px";
+  div.style.border = "1px solid black";
+  div.style.borderRadius = "10px";
+
   document.body.className = "has-modal";
 
   emailWindow = div;
-  document.body.append(div);
+  document.body.appendChild(div);
+
   function remove() {
     document.body.classList.remove("has-modal");
     document.body.removeChild(emailWindow);
     emailWindow = null;
   }
-  div.querySelector(".close").onclick = remove;
+
+  // div.querySelector(".close").onclick = remove;
 }
